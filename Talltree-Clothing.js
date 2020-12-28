f014e2a3929101ff449aa76e031bd59c6728c961
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2020-12-28 00:17:56 -0600

    Talltree Clothing Co. Part 13

--- Talltree-Clothing.js
+++ Talltree-Clothing.js
@@ -6,3 +6,12 @@ function myProducts() {
 function submitEmail() {
     document.getElementById("newsLetter").classList.toggle("submit");
 }
+// $( "select" )
+//     .change(function () {
+//         var str = "";
+//         $( "select option:selected" ).each(function() {
+//             str += $( this ).text() + " ";
+//         });
+//         $( "div" ).text( str );
+//     })
+//     .change();
